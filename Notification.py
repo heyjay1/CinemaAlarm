3416891a7ee6a36155759997eaedda44cc614559
jay <heyjay_@naver.com>
2022-05-02 19:43:29 +0900

    Add Main in Notification.py and Add SendAlarm in TelegramBot.py

--- Notification.py
+++ Notification.py
@@ -1,27 +1,59 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
+import privates
+import TelegramBot
+import schedule
 
-html = urlopen("https://linktr.ee/emucinema")
-bsObject = BeautifulSoup(html, "html.parser")
+def ScrapHTML():
+    html = urlopen("https://linktr.ee/emucinema")
+    bsObject = BeautifulSoup(html, "html.parser")
 
-#html에서 원하는 json부분 추출
-data = {}
-data = bsObject.find("script", {"id": "__NEXT_DATA__"})
+    #html에서 원하는 json부분 추출
+    data = {}
+    data = bsObject.find("script", {"id": "__NEXT_DATA__"})
 
-#string 다듬기
-start = str(data).find('>')
-end = str(data).find('</script>')
+    return data
 
-processedData = str(data)[start : end]
+def MakeJSONData(data):
+    #string 다듬기
+    start = str(data).find('>')
+    end = str(data).find('</script>')
 
-processedData = processedData.strip('<>')
+    processedData = str(data)[start : end]
 
-#json object로 디코딩
-json_obj = json.loads(processedData)
+    processedData = processedData.strip('<>')
 
-#json에서 원하는 부분을 배열로 받기
-link_array = json_obj['props']['pageProps']['account']['links']
+    #json object로 디코딩
+    json_obj = json.loads(processedData)
 
-for link in link_array:
-    print(link)
+    return json_obj
+
+def scrap():
+    data = ScrapHTML()
+    json_obj = MakeJSONData(data)
+
+    #json에서 원하는 부분을 배열로 받기
+    link_array = json_obj['props']['pageProps']['account']['links']
+
+    new_title = []
+    new_id = []
+    for link in link_array:
+        new_title.append(link['title'])
+        new_id.append(link['id'])
+    print(new_title, new_id)
+
+    if (new_title != privates.link_title) or (new_id != privates.link_id):
+        TelegramBot.SendAlarm(link_array, True)
+    else:
+        TelegramBot.SendAlarm(link_array, False)
+
+def main():
+    schedule.every(1).minutes.do(scrap)
+    while True:
+        schedule.run_pending()        
+
+if __name__ == '__main__':
+    main()
+
+#3. 1분단위로 스크롤링?
--- TelegramBot.py
+++ TelegramBot.py
@@ -2,7 +2,8 @@ import telegram
 import privates
 
 token = privates.token
-id = privates.id
+myId = privates.myId
+all_client_id = privates.all_client_id
 
 bot = telegram.Bot(token = token)
 updates = bot.getUpdates()
@@ -10,4 +11,16 @@ updates = bot.getUpdates()
 for u in updates:
     print(u)
 
-bot.sendMessage(chat_id = id, text = 'test')
+
+def SendAlarm(links, bAll):
+    strMessage = ''
+    for link in links:
+        strMessage += link['title'] + ' | ' + link['url'] + '\n'
+
+    if bAll:
+        for id in all_client_id:
+            bot.sendMessage(chat_id = id, text = strMessage)
+    else:
+        bot.sendMessage(chat_id=myId, text=strMessage)
+
+#1. 입장 아이디를 자동적으로 수집
